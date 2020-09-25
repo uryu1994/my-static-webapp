050d85807e55d1db9f2d61d53ea117f6f421163e
uryu1994 <prices_over@uryu1994.work>
2020-09-25 18:03:17 +0900

    Add sample API

---
+++ my-static-webapp_api/GetWeatherList.cs
@@ -0,0 +1,38 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using my_static_webapp_domain;
+
+namespace my_static_webapp_api
+{
+    public static class GetWeatherList
+    {
+        [FunctionName("GetWeatherList")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+            var responseData = new List<WeatherForecast>
+            {
+                { new DateTime(2018, 5, 6), 1, "Freezing" },
+                { new DateTime(2018, 5, 7), 14, "Bracing" },
+                { new DateTime(2018, 5, 8), -13, "Freezing"},
+                { new DateTime(2018, 5, 9), -16, "Balmy"},
+                { new DateTime(2018, 5, 10), -2, "Chilly" }
+            };
+
+            return new OkObjectResult(JsonConvert.SerializeObject(responseData));
+        }
+    }
+}
---
+++ my-static-webapp_domain/WeatherForecast.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace my_static_webapp_domain
+{
+    public class WeatherForecast
+    {
+        public WeatherForecast()
+        {
+        }
+
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+
+    public static class WeatherForecastExtensions
+    {
+        public static void Add(this List<WeatherForecast> list, DateTime date, int temperature_c, string summary)
+        {
+            list.Add(new WeatherForecast
+            {
+                Date = date,
+                TemperatureC = temperature_c,
+                Summary = summary
+            });
+        }
+    }
+}
